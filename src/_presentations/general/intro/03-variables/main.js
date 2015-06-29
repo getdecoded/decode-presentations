c543329e6cae8cff56c911b9f7917a6ed5a41f67
BuildTools <christian@decode.org.nz>
2015-06-30 11:22:05 +1200

    Add variables presentation

---
+++ src/_presentations/general/intro/03-variables/main.js
@@ -0,0 +1,18 @@
+var Decode = require('./src/assets/scripts/main.js');
+var $ = require('jquery');
+
+Decode.addStarter();
+
+Decode.presentation
+  .step('.starter-slide', 'slide', { reverse: true })
+  .milestone('Road');
+
+$('.road-container .building').css({ scale: 1.2, opacity: 0 });
+
+Decode.presentation
+  .step('.road-container .car', 'animatecss', { props: { opacity: 1, y: -200 } })
+  .step('.road-container .car', 'animatecss', { props: { y: -600 }, duration: 1500 })
+  .and('.road-label', 'fade', { delay: 1000 });
+
+Decode.presentation.step('.road-container .building.first', 'animatecss', { props: { scale: 1, opacity: 1 } })
+  .and('.road-container .building.second', 'animatecss', { props: { scale: 1, opacity: 1 }, delay: 200 });
